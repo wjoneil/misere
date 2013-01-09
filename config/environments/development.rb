83c6a8539b5061393e8b586738a11133368d5f26
William O'Neil <will@sortofgood.com>
2013-01-10 00:47:10 +1100

    typekit support, plus some more secret config stuff. Added an example file.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,6 @@ Misere::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "robot@misere.co.nz"
+  config.mailer_sender = "will@heywill.com"
 
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer"
