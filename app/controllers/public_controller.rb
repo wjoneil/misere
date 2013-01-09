2aadc5c860ea4ef4d2aa5280c65f51356f2bc02f
William O'Neil <will@sortofgood.com>
2013-01-10 01:08:49 +1100

    added redirect in public controller for logged in users

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,21 @@
 class PublicController < ApplicationController
 
   def index
+
+    if user_signed_in?
+
+      url = if current_user.players.length < 4
+        players_url
+      elsif current_user.teams.length < 2
+        teams_url
+      else
+        games_url
+      end
+
+      redirect_to url
+      return
+    end
+
     respond_to do |format|
       format.html # index.html.erb
     end
